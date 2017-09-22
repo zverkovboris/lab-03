c2404b88fb3ec1dc9ce977f72c680e247ce365cf
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2017-09-22 10:56:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Pi=" << 3.14 << std::endl;//print pi
+    char c= ' ';
+    std:: cout << 1 << c << 13 << c << 49 << std::endl;// print 1 13 49
+    char b;
+    std::cin >> b;
+    std::cout << "Ввести символ" << std::endl;
+    std:: cout << 1 << b << 13 << b << 49 << std::endl;//print 1 13 49 with symbol between
+    int a1,a2,a3;
+
+    std::cin >> a1;
+    std::cin >> a2;
+    std::cin >> a3;
+    char c='  ';
+
+
+      return 0;
+}
