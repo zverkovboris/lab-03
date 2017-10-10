3bb4e42a34e02d0b15933a7f08f54eef374e8906
zverkovboris <32186297+zverkovboris@users.noreply.github.com>
2017-10-10 19:12:48 +0300

    Update classwork.cpp

--- classwork.cpp
+++ classwork.cpp
@@ -1,190 +1,308 @@
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <math.h>
+#include <random>
+#include <string>
+#include <vector>
 
-int main() {
-    // variant7
-    char n = 0;
-    do
+
+template<class T>
+std::vector<T> generate_random_vector(std::size_t num, T min = -10, T max = 10)
+{
+    std::random_device rd;
+    std::mt19937 e2(rd());
+    std::uniform_real_distribution<> dist(min, max);
+
+    auto lambda = [&e2, &dist] () -> T { return dist(e2); };
+
+    std::vector<T> result(num);
+    std::generate_n(result.begin(), num, lambda);
+    return result;
+}
+
+
+int main()
+{
+    std::vector <int> v = generate_random_vector <int> (10, -100, 50);
+    for (int i = 0; i < 10; ++i)
     {
-        std::cout << "What chess piece?" << std::endl;
-        std::cin >> n;
-    } while ((n != 'p') && (n != 'N') && (n != 'Q') && (n != 'R') && (n != 'B'));
-    std::pair<char, int> myf;
-    do
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+    // *2
+    for (int i = 0; i < 10; ++i)
     {
-        std::cout << "initial coordinate?" << std::endl;
-        std::cin >> myf.first >> myf.second;
-    } while ((myf.second > 8) || ((myf.first != 'a') && (myf.first != 'b') && (myf.first != 'c') &&
-            (myf.first != 'd') && (myf.first != 'e') && (myf.first != 'f') && (myf.first != 'h')));
-    std::pair<char, int> nex;
-    do
+        std::cout << v[i]*2 << " ";
+    }
+    std::cout << std::endl;
+    // -a
+    std::cout << "Print a" << std::endl;
+    int a = 0;
+    std::cin >> a;
+    for (int i = 0; i < 10; ++i)
     {
-        std::cout << "next coordinate?" << std::endl;
-        std::cin >> nex.first >> nex.second;
-    } while ((myf.second > 8) || ((myf.first != 'a') && (myf.first != 'b') && (myf.first != 'c') &&
-            (myf.first != 'd') && (myf.first != 'e') && (myf.first != 'f') && (myf.first != 'h')));
-    std::cout << "Answer: " << std::endl;
-    if (n == 'p')
+        std::cout << v[i] - a << " ";
+    }
+    std::cout << std::endl;
+    // /v[0]
+    float b = v[0];
+    for (int i = 0; i < 10; ++i)
     {
-        if (((nex.first == myf.first) && (nex.second - 1 == myf.second)) &&
-            ((nex.first != 'f') && (myf.second != 7))) 
+        std::cout << v[i]/b << " ";
+    }
+    std::cout << std::endl;
+    // summ
+    a = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        a += v[i];
+    }
+    std::cout << "summ =" << a << std::endl;
+    // summ ^2
+    a = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        a += v[i]*v[i];
+    }
+    std::cout << "summ =" << a << std::endl;
+    // summ 6
+    a = 0;
+    for (int i = 0; i < 6; ++i)
+    {
+        a += v[i];
+    }
+    std::cout << "summ =" << a << std::endl;
+    // summ from k1 to k2
+    std::cout << "print k1 and k2, k2>k1" << std::endl;
+    unsigned int k1 = 0;
+    unsigned int k2 = 0;
+    std:: cin >> k1 >> k2;
+    a = 0;
+    for (unsigned int i = k1 - 1; i < k2; ++i)
+    {
+        a += v[i];
+    }
+    std::cout << "summ =" << a << std::endl;
+    // the arithmetic mean
+    a = 0;
+    b = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        b +=1;
+        a += v[i];
+    }
+    std::cout << "the arithmetic mean =" << a/b << std::endl;
+    // arifm mean from s1 to s2
+    std::cout << "print s1 and s2, s2>s1" << std::endl;
+    unsigned int s1 = 0;
+    unsigned int s2 = 0;
+    std:: cin >> s1 >> s2;
+    a = 0;
+    b = 0;
+    for (unsigned int i = s1 - 1; i < s2; ++i)
+    {
+        b += 1;
+        a += v[i];
+    }
+    std::cout << "the arithmetic mean =" << a/b << std::endl;
+    // change places 3 elements
+    int q[20];
+    for (int i = 0; i < 20; ++i)
+    {
+        q[i] = rand()%100;
+        std::cout << q[i] << " ";
+    }
+    std::cout << std::endl;
+    a = 0;
+    for (int i = 0; i < 20; ++i)
+    {
+        if(i < 3)
         {
-            std::cout << "True" << std::endl;
-        } else if ((myf.second == 2) && (nex.second - 2 == myf.second) &&
-                   (nex.first == myf.second))
+            a = q[i];
+            q[i] = q[17+i];
+            q[17+i] = a;
+        }
+        std::cout << q[i] << " ";
+    }
+    std::cout << std::endl;
+    // delete first <0
+    a = 0;
+    for (int i = 0; i < 20; ++i)
+    {
+        if (a == 0)
         {
-            std::cout << "True" << std::endl;
-        } else {
-            std::cout << "false" << std::endl;
+            if (q[i] <0)
+            {
+                a = 1;
+            }
+        }
+        if ((a == 1) && (i <19))
+        {
+            std::cout << q[i+1] << " ";
+        } 
+        else if (a == 0)
+        {
+            std::cout << q[i] << " ";
         }
     }
-    if (n=='R')
+    // delete first <0
+    int n =0;
+    if (a == 1)
     {
-        if ((nex.first == myf.first) || (nex.second == myf.second))
+        n = 19;
+    }
+    else
+    {
+        n = 20;
+    }
+    a = 0; 
+    std::cout << std::endl;
+    for (int i = 0; i < n; ++i)
+    {
+        if (a == 0)
         {
-            if ((myf.first == 'f') && (nex.first == 'f'))
+            if (q[i] % 2 == 0)
             {
-                if ((nex.second > 7) && (myf.second < 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second < 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second > 7) && (myf.second > 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second > 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
+                a = 1;
             }
-            if ((myf.first == 7) && (nex.first == 7))
-            {
-                if ((myf.first < 'f') && (nex.first < 'f'))
-                {
-                    std::cout << "true" << std::endl;
-                }
-                if ((nex.first > 'f') && (myf.first > 'f'))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.first > 'f') && (myf.first < 'f'))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.first < 'f') && (myf.first > 'f'))
-                {
-                    std::cout << "false" << std::endl;
-                }
-            }
-            std::cout << "True" << std::endl;
+        }
+        if ((a == 1) && (i < n-1))
+        {
+            std::cout << q[i+1] << " ";
+        } 
+        else if (a == 0)
+        {
+            std::cout << q[i] << " ";
+        }
+    }
+    std::cout << std::endl;
+    // city
+    std::cout << "Print city" << std::endl;
+    std::string c1 = "0";
+    std::cin >> c1;
+    std::cout << "City name " << c1.size() << std::endl;
+    // length second name
+    std::cout << "Print second names" << std::endl;
+    std::string f1 = "0";
+    std::string f2 = "0";
+    std::cin >> f1 >> f2;
+    if (f1.size() > f2.size())
+    {
+        std::cout << "First second name longer" << std::endl;
+    }
+    else if (f1.size() < f2.size())
+    {
+        std::cout << "The second second name longer" << std::endl;
+    } else
+    {
+        std::cout << "second names have the same length" << std::endl;
+    }
+    // cities
+    std::cout << " Print 3 cities" << std::endl;
+    std::string c2 = "0";
+    std::string c3 = "0";
+    std::cin >> c1 >> c2 >> c3;
+    if ((c1.size() >= c2.size()) && (c1.size() >= c3.size()))
+    {
+        if (c2.size() >= c3.size())
+        {
+            std::cout << c1 << " " << c3 << std::endl;
         }
         else
         {
-            std::cout << "false" << std::endl;
+            std::cout << c1 << " " << c2 << std::endl;
         }
     }
-    if (n=='B')
+    if ((c2.size() > c1.size()) && (c2.size() >= c3.size()))
     {
-        if (abs(myf.first - myf.first) == abs(nex.second - myf.second))
+        if (c1.size() >= c3.size())
         {
-            if (abs(nex.first - 'f') == abs(nex.second - 7))
-            {
-                if ((nex.second > 7) && (myf.second < 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second < 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second > 7) && (myf.second > 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second > 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-            }
-            std::cout << "True" << std::endl;
+            std::cout << c2 << " " << c3 << std::endl;
         }
         else
         {
-            std::cout << "True" << std::endl;
+            std::cout << c2 << " " << c1 << std::endl;
         }
     }
-    if (n=='Q')
+    if ((c3.size() > c1.size()) && (c3.size() > c2.size()))
     {
-        if ((abs(myf.first - myf.first) == abs(nex.second - myf.second)) ||
-            ((nex.first == myf.first) || (nex.second == myf.second)))
+        if (c1.size() >= c2.size())
         {
-            if ((myf.first == 'f') && (nex.first == 'f'))
-            {
-                if ((nex.second > 7) && (myf.second < 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second < 7)
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second > 7) && (myf.second > 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second > 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-            }
-            if ((myf.first == 7) && (nex.first == 7)) {
-                if ((myf.first < 'f') && (nex.first < 'f'))
-                {
-                    std::cout << "true" << std::endl;
-                }
-                if ((nex.first > 'f') && (myf.first > 'f'))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.first > 'f') && (myf.first < 'f'))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.first < 'f') && (myf.first > 'f'))
-                {
-                    std::cout << "false" << std::endl;
-                }
-            }
-            if (abs(nex.first - 'f') == abs(nex.second - 7))
-            {
-                if ((nex.second > 7) && (myf.second < 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second < 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second > 7) && (myf.second > 7))
-                {
-                    std::cout << "True" << std::endl;
-                }
-                if ((nex.second < 7) && (myf.second > 7))
-                {
-                    std::cout << "false" << std::endl;
-                }
-            }
-            std::cout << "True" << std::endl;
+            std::cout << c3 << " " << c2 << std::endl;
         }
         else
         {
-            std::cout << "True" << std::endl;
+            std::cout << c3 << " " << c1 << std::endl;
         }
     }
+    // word m n
+    int m = 0;
+    n = 0;
+    do
+    {
+        std::cout << "Print word and m,n" << std::endl;
+        std:: cin >> c1 >> m >> n;
+    } while ((c1.size() < n) && (m > n));
+    for (int i = m - 1; i < n; ++i)
+    {
+        std::cout << c1[i];
+    }
+    std::cout << std::endl;
+    a = c1.size();
+    for (int i = 0; i < a; ++i)
+    {
+        c1.push_back('*');
+        c1.insert(c1.begin(),'*');
+    }
+    std::cout << c1 << std::endl;
+    // sentence
+    std::cout << "Print sentence: ";
+    // fflush (stdin);
+    std::cin.clear();
+    std::getline(std::cin,c1);
+    a = c1.size();
+    b = 0;
+    for (int i = 0; i < a; ++i)
+    {
+        if (c1[i] == 'a')
+        {
+            b += 1;
+        }
+    }
+    std::cout << (100*b)/a << "% letter a in this sentence" << std::endl;
+    // change can
+    std::cin.clear();
+    c1 = "Can you can a can as a canner can can a can?";
+    std::cout << "Print word that you want to see(not can) ";
+    
+    std::cin >> c2;
+    n = -1;
+    n = c2.find("can");
+    if (n != -1)
+    {
+        std::cout << "ERROR!!!!! infinity cycle" << std:: endl;
+    }
+    else
+    {
+        m = c2.size();
+        n = 0;
+        int o =0;
+        do
+        {
+            o = c1.find (" can ");
+            if (o == 0)
+            {
+                std::size_t o = c1.find (" can?");
+                
+            }
+            if ((n == 0) || ( o > n+m))
+            {
+                n = o;
+                c1.replace (o+1, o+4, c2);
+            }
+        } while ( o != 0);
+    }
+    std::cout << c1 << std::endl;
     return 0;
-
 }
